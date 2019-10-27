5f921edf288d1b09fd1cdfc425736af96aa27f4a
Ahmed <ahmed91abbas@hotmail.com>
2019-10-27 13:04:33 +0100

    Add queue to avoid duplicate events

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import slack
 import os
 import json
 from environs import Env
+from collections import deque
 
 env = Env()
 env.read_env()
@@ -11,14 +12,18 @@ slack_events_adapter = SlackEventAdapter(
     env("SLACK_SIGNING_SECRET"), "/slack/events")
 slack_client = slack.WebClient(env("BOT_ACCESS_TOKEN"))
 
+# Used to prevent responding to events several times
+ts_queue = deque([], maxlen=100)
+
 # Message events
 @slack_events_adapter.on("message")
 def handle_message(event_data):
     message = event_data["event"]
     if is_valid_message(message):
+        ts_queue.appendleft(message["ts"])
         message_text = message["text"]
         channel = message["channel"]
-        if "hi" in message_text.lower():
+        if "hi" in message_text.lower().split(" "):
             response_message = "Hello <@%s>! :tada:" % message["user"]
             response = slack_client.chat_postMessage(channel=channel, text=response_message)
 
@@ -30,6 +35,7 @@ def error_handler(err):
 def is_valid_message(message):
     #CPW9ATAF9 = General channel
     return (message.get("subtype") is None) \
-        and (message["channel"] != "CPW9ATAF9")
+        and (message["channel"] != "CPW9ATAF9") \
+        and (not message["ts"] in ts_queue)
 
 slack_events_adapter.start(port=env("PORT"))
