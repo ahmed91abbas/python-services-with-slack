758772e2e24e102224ecf5f6914feae8a9ec03bd
Ahmed <ahmed91abbas@hotmail.com>
2019-10-27 08:56:06 +0100

    Add method to validate message

--- app.py
+++ app.py
@@ -15,14 +15,21 @@ slack_client = slack.WebClient(env("BOT_ACCESS_TOKEN"))
 @slack_events_adapter.on("message")
 def handle_message(event_data):
     message = event_data["event"]
-    if message.get("subtype") is None and "hi" in message.get('text'):
+    if is_valid_message(message):
+        message_text = message["text"]
         channel = message["channel"]
-        message = "Hello <@%s>! :tada:" % message["user"]
-        response = slack_client.chat_postMessage(channel=channel, text=message)
+        if "hi" in message_text.lower():
+            response_message = "Hello <@%s>! :tada:" % message["user"]
+            response = slack_client.chat_postMessage(channel=channel, text=response_message)
 
 # Error events
 @slack_events_adapter.on("error")
 def error_handler(err):
     print("ERROR: " + str(err))
+
+def is_valid_message(message):
+    #CPW9ATAF9 = General channel
+    return (message.get("subtype") is None) \
+        and (message["channel"] != "CPW9ATAF9")
 
 slack_events_adapter.start(port=env("PORT"))
