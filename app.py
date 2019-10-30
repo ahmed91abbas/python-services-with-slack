550c2d8c70913d3f96cb39c7b0278ceb2e26319f
Ahmed <ahmed91abbas@hotmail.com>
2019-10-30 19:41:34 +0100

    Check res msg type before sending post

--- app.py
+++ app.py
@@ -31,7 +31,10 @@ def handle_message(event_data):
         else:
             response_message = 'No service found for your text! Type "Help" \
                 to get a list of the available services'
-        response = slack_client.chat_postMessage(channel=channel, **response_message)
+        if type(response_message) is dict:
+            response = slack_client.chat_postMessage(channel=channel, **response_message)
+        else:
+            response = slack_client.chat_postMessage(channel=channel, text=response_message)
 
 # Error events
 @slack_events_adapter.on("error")
