c09a7b35df10ecaf8822e1caf0f14535305783d0
Ahmed <ahmed91abbas@hotmail.com>
2019-11-09 17:14:26 +0100

    Change from pyDictionary to use nltk wordnet

--- services/dictionary.py
+++ services/dictionary.py
@@ -3,41 +3,54 @@ import sys
 sys.path.append(os.path.abspath('..'))
 
 from services.utils.slack_message_builder import Slack_message_builder
-from PyDictionary import PyDictionary
 import re
+import nltk
+try:
+    nltk.data.find('tokenizers/wordnet')
+except LookupError:
+    nltk.download('wordnet')
+from nltk.corpus import wordnet
 
 
 class Dictionary:
 
     def __init__(self, *args):
-        self.action_list = self.init_action_list()
-        self.dictionary = PyDictionary()
+        pass
 
-    def init_action_list(self):
-        action_list = {}
-
-        action_list["meaning"] = {}
-        action_list["meaning"]["regex"] = '(?:dict|dictionary) meaning of (.*)'
-        action_list["meaning"]["function"] = self.get_meaning
-
-        return action_list
-
-    def get_meaning(self, text):
-        return self.dictionary.meaning(text)
+    def get_word_information(self, text):
+        syn = set()
+        ant = set()
+        synsets = wordnet.synsets(text)
+        if not synsets:
+            return {"error":True, "word":text}
+        definition = synsets[0].definition()
+        examples = synsets[0].examples()
+        for synset in synsets:
+            for lemma in synset.lemmas():
+                syn.add(lemma.name())
+                if lemma.antonyms():
+                    ant.add(lemma.antonyms()[0].name())
+        return {"error":False, "word":text, "definition":definition, \
+            "examples":examples, "synonyms":list(syn), "antonyms":list(ant)}
 
     def build_response_message(self, text, **kwargs):
         result = self.parse_message(text)
 
+        if result["error"]:
+            return "No results found for " + result["word"]
+
         smb = Slack_message_builder()
-        if type(result) is dict:
-            for key, value in result.items():
-                smb.add_plain_section(f'*{key}*')
-                smb.add_formated_section("\n".join(value))
-                smb.add_divider()
-        elif type(result) is list:
-            smb.add_formated_section("\n".join(value))
-        else:
-            smb.add_plain_section(result)
+        smb.add_formated_section(f'*{result["word"]}*')
+        smb.add_plain_section(result["definition"])
+        smb.add_divider()
+        if result["examples"]:
+            smb.add_formated_section('*Examples:*\n' + "\n".join(result["examples"]))
+            smb.add_divider()
+        if result["synonyms"]:
+            smb.add_formated_section(f'*Synonyms:* {" ,".join(result["synonyms"])}')
+            smb.add_divider()
+        if result["antonyms"]:
+            smb.add_formated_section(f'*Antonyms:* {" ,".join(result["antonyms"])}')
         return smb.message
 
     def get_match(self, regex, text):
@@ -45,14 +58,13 @@ class Dictionary:
         return p.match(text)
 
     def parse_message(self, text):
-        for key, value in self.action_list.items():
-            m = self.get_match(value["regex"], text)
-            if m:
-                return value["function"](m.group(1))
+        m = self.get_match('(?:dict|dictionary) (.*)', text)
+        if m:
+            return self.get_word_information(m.group(1))
         return "No results found."
 
 if __name__ == '__main__':
-    text = "dict meaning of gain"
+    text = "dict subsidydsa"
     dict_service = Dictionary()
     res = dict_service.build_response_message(text=text)
     if type(res) is dict:
