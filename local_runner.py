46210ad2262131cf41e9fe6aa063b1a6c0b3d347
Ahmed <ahmed91abbas@hotmail.com>
2019-11-23 16:20:24 +0100

    Dynamic call of services in local runner

--- local_runner.py
+++ local_runner.py
@@ -6,6 +6,10 @@ from datetime import datetime
 import json
 import tkinter as tk
 import sys
+import importlib
+import glob
+import re
+import os
 
 
 class Local_runner:
@@ -24,12 +28,19 @@ class Local_runner:
 
     def init_services_dict(self):
         services = {}
-        services["Prayer Times"] = self.call_prayer_times
-        services["Todo List"] = self.call_todo_list
-        services["Reminder"] = self.call_reminder
-        services["Wikipedia Summary"] = self.call_wikipedia_summary
-        services["Dictionary"] = self.call_dictionary
-        services["Audio Fetcher"] = self.call_audio_fetcher
+        root = "services"
+        for filename in glob.glob(os.path.join(root, "*.py")):
+            module_name = re.sub(r"\\+", ".", filename.replace(".py", ""))
+            class_name = module_name.replace(f"{root}.", "").capitalize()
+            key = ""
+            for part in class_name.split("_"):
+                key += f"{part.capitalize()} "
+            key = key.strip()
+
+            services[key] = {}
+            services[key]["module_name"] = module_name
+            services[key]["class_name"] = class_name
+
         return services
 
     def set_service_name(self, name):
@@ -130,7 +141,7 @@ class Local_runner:
         self.message = self.message_entry.get()
         self.event = self.slack_event_mock(self.message)
         if self.service_name in self.services_dict:
-            response = self.services_dict[self.service_name]()
+            response = self.call_service()
             self.response_text.insert(tk.END, self.to_string(response))
 
     def get_default_values(self):
@@ -151,57 +162,12 @@ class Local_runner:
         with open(self.default_values_file, 'w') as outfile:
             json.dump(default_values, outfile)
 
-    def call_prayer_times(self):
-        import services.prayer_times as prayer_times
-        import importlib
-        importlib.reload(prayer_times)
+    def call_service(self):
+        service_dict = self.services_dict[self.service_name]
+        module = importlib.import_module(service_dict["module_name"])
+        importlib.reload(module)
 
-        service = prayer_times.Prayer_times(self.env)
-        message = service.build_response_message(**self.event)
-        return message
-
-    def call_todo_list(self):
-        import services.todo_list as todo_list
-        import importlib
-        importlib.reload(todo_list)
-
-        service = todo_list.Todo_list(self.env)
-        message = service.build_response_message(**self.event)
-        return message
-
-    def call_reminder(self):
-        import services.reminder as reminder
-        import importlib
-        importlib.reload(reminder)
-
-        service = reminder.Reminder(self.env)
-        message = service.build_response_message(**self.event)
-        return message
-
-    def call_wikipedia_summary(self):
-        import services.wikipedia_summary as wikipedia_summary
-        import importlib
-        importlib.reload(wikipedia_summary)
-
-        service = wikipedia_summary.Wikipedia_summary(self.env)
-        message = service.build_response_message(**self.event)
-        return message
-
-    def call_dictionary(self):
-        import services.dictionary as dictionary
-        import importlib
-        importlib.reload(dictionary)
-
-        service = dictionary.Dictionary(self.env)
-        message = service.build_response_message(**self.event)
-        return message
-
-    def call_audio_fetcher(self):
-        import services.audio_fetcher as audio_fetcher
-        import importlib
-        importlib.reload(audio_fetcher)
-
-        service = audio_fetcher.Audio_fetcher(self.env)
+        service = getattr(module, service_dict["class_name"])(self.env)
         message = service.build_response_message(**self.event)
         return message
 
